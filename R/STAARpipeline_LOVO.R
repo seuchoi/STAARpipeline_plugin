1f36e5c5cd826b025b36e6b602836daa72101609
seuchoi <seuchoi@gmail.com>
2022-12-06 12:53:34 -0500

    upload leave one variant out analysis.

---
+++ R/STAARpipeline_LOVO.R
@@ -0,0 +1,48 @@
+STAARpipeline_LOVO<-function(genofile,genes,category,Geno,obj_nullmodel,Anno.Int.PHRED.sub,rare_maf_cutoff,rv_num_cutoff,silent){
+
+  ## leave one variant out analysis start
+  results <- c()
+  variant_info<-variantInfo(genofile)
+  varids<-paste(variant_info$chr,variant_info$pos,variant_info$ref,variant_info$alt,sep=":")
+
+  for (lvar in 0:length(varids)){
+  pvalues <- NA
+
+  if(lvar==0){
+
+  try(pvalues <- STAAR(Geno,obj_nullmodel,Anno.Int.PHRED.sub,rare_maf_cutoff=rare_maf_cutoff,rv_num_cutoff=rv_num_cutoff),silent=silent)
+  rmvar<-"None"
+  }else{
+  cat("  remove number",lvar,"variant out of \n")
+  Geno2<-Geno[,-c(lvar)]
+  Anno.Int.PHRED.sub2<-Anno.Int.PHRED.sub[-c(lvar),]
+  rmvar<-varids[lvar]
+  try(pvalues <- STAAR(Geno2,obj_nullmodel,Anno.Int.PHRED.sub2,rare_maf_cutoff=rare_maf_cutoff,rv_num_cutoff=rv_num_cutoff),silent=silent)
+
+  }
+	if(class(pvalues)=="list")
+	{
+		results_temp <- as.vector(genes[kk,])
+		results_temp[3] <- category
+		results_temp[2] <- chr
+		results_temp[1] <- as.character(genes[kk,1])
+    results_temp[4] <- rmvar
+    results_temp[5] <- pvalues$num_variant
+
+
+		results_temp <- c(results_temp,pvalues$results_STAAR_S_1_25,pvalues$results_STAAR_S_1_1,
+		pvalues$results_STAAR_B_1_25,pvalues$results_STAAR_B_1_1,pvalues$results_STAAR_A_1_25,
+		pvalues$results_STAAR_A_1_1,pvalues$results_ACAT_O,pvalues$results_STAAR_O)
+
+		results <- rbind(results,results_temp)
+	}
+  }
+
+	if(!is.null(results))
+	{
+		colnames(results) <- colnames(results, do.NULL = FALSE, prefix = "col")
+		colnames(results)[1:5] <- c("Gene name","Chr","Category","rm_variant","#SNV")
+		colnames(results)[(dim(results)[2]-1):dim(results)[2]] <- c("ACAT-O","STAAR-O")
+	}
+
+}
---
+++ R/disruptive_missense_LOVO.R
@@ -0,0 +1,129 @@
+disruptive_missense_LOVO <- function(chr,gene_name,genofile,obj_nullmodel,genes,
+                                rare_maf_cutoff=0.01,rv_num_cutoff=2,
+                                QC_label="annotation/filter",variant_type=c("SNV","Indel","variant"),geno_missing_imputation=c("mean","minor"),
+                                Annotation_dir="annotation/info/FunctionalAnnotation",Annotation_name_catalog,
+                                Use_annotation_weights=c(TRUE,FALSE),Annotation_name=NULL,silent=FALSE){
+
+	## evaluate choices
+	variant_type <- match.arg(variant_type)
+	geno_missing_imputation <- match.arg(geno_missing_imputation)
+
+	phenotype.id <- as.character(obj_nullmodel$id_include)
+
+	## get SNV id, position, REF, ALT (whole genome)
+	filter <- seqGetData(genofile, QC_label)
+	if(variant_type=="variant")
+	{
+		SNVlist <- filter == "PASS"
+	}
+
+	if(variant_type=="SNV")
+	{
+		SNVlist <- (filter == "PASS") & isSNV(genofile)
+	}
+
+	if(variant_type=="Indel")
+	{
+		SNVlist <- (filter == "PASS") & (!isSNV(genofile))
+	}
+
+	position <- as.numeric(seqGetData(genofile, "position"))
+	variant.id <- seqGetData(genofile, "variant.id")
+
+	rm(filter)
+	gc()
+
+	### Gene
+	kk <- which(genes[,1]==gene_name)
+
+	sub_start_loc <- genes[kk,3]
+	sub_end_loc <- genes[kk,4]
+
+	is.in <- (SNVlist)&(position>=sub_start_loc)&(position<=sub_end_loc)
+	variant.id.gene <- variant.id[is.in]
+
+	seqSetFilter(genofile,variant.id=variant.id.gene,sample.id=phenotype.id)
+
+	## disruptive_missense
+	## Gencode_Exonic
+	GENCODE.EXONIC.Category <- seqGetData(genofile, paste0(Annotation_dir,Annotation_name_catalog$dir[which(Annotation_name_catalog$name=="GENCODE.EXONIC.Category")]))
+	## Meta.SVM.Pred
+	MetaSVM_pred <- seqGetData(genofile, paste0(Annotation_dir,Annotation_name_catalog$dir[which(Annotation_name_catalog$name=="MetaSVM")]))
+
+	variant.id.gene <- seqGetData(genofile, "variant.id")
+	lof.in.ds <- ((GENCODE.EXONIC.Category=="nonsynonymous SNV")&(MetaSVM_pred=="D"))
+	variant.id.gene <- variant.id.gene[lof.in.ds]
+
+	seqSetFilter(genofile,variant.id=variant.id.gene,sample.id=phenotype.id)
+	## genotype id
+	id.genotype <- seqGetData(genofile,"sample.id")
+	# id.genotype.match <- rep(0,length(id.genotype))
+
+	id.genotype.merge <- data.frame(id.genotype,index=seq(1,length(id.genotype)))
+	phenotype.id.merge <- data.frame(phenotype.id)
+	phenotype.id.merge <- dplyr::left_join(phenotype.id.merge,id.genotype.merge,by=c("phenotype.id"="id.genotype"))
+	id.genotype.match <- phenotype.id.merge$index
+
+	## Genotype
+	Geno <- seqGetData(genofile, "$dosage")
+	Geno <- Geno[id.genotype.match,,drop=FALSE]
+
+	## impute missing
+	if(!is.null(dim(Geno)))
+	{
+		if(dim(Geno)[2]>0)
+		{
+			if(geno_missing_imputation=="mean")
+			{
+				Geno <- matrix_flip_mean(Geno)$Geno
+			}
+			if(geno_missing_imputation=="minor")
+			{
+				Geno <- matrix_flip_minor(Geno)$Geno
+			}
+		}
+	}
+
+	## Annotation
+	Anno.Int.PHRED.sub <- NULL
+	Anno.Int.PHRED.sub.name <- NULL
+
+	if(variant_type=="SNV")
+	{
+		if(Use_annotation_weights)
+		{
+			for(k in 1:length(Annotation_name))
+			{
+				if(Annotation_name[k]%in%Annotation_name_catalog$name)
+				{
+					Anno.Int.PHRED.sub.name <- c(Anno.Int.PHRED.sub.name,Annotation_name[k])
+					Annotation.PHRED <- seqGetData(genofile, paste0(Annotation_dir,Annotation_name_catalog$dir[which(Annotation_name_catalog$name==Annotation_name[k])]))
+
+					if(Annotation_name[k]=="CADD")
+					{
+						Annotation.PHRED[is.na(Annotation.PHRED)] <- 0
+					}
+
+					if(Annotation_name[k]=="aPC.LocalDiversity")
+					{
+						Annotation.PHRED.2 <- -10*log10(1-10^(-Annotation.PHRED/10))
+						Annotation.PHRED <- cbind(Annotation.PHRED,Annotation.PHRED.2)
+						Anno.Int.PHRED.sub.name <- c(Anno.Int.PHRED.sub.name,paste0(Annotation_name[k],"(-)"))
+					}
+					Anno.Int.PHRED.sub <- cbind(Anno.Int.PHRED.sub,Annotation.PHRED)
+				}
+			}
+
+			Anno.Int.PHRED.sub <- data.frame(Anno.Int.PHRED.sub)
+			colnames(Anno.Int.PHRED.sub) <- Anno.Int.PHRED.sub.name
+		}
+	}
+
+
+  ## leave one variant out analysis start
+  result2<-STAARpipeline_LOVO(genofile,genes,category,Geno,obj_nullmodel,Anno.Int.PHRED.sub,rare_maf_cutoff,rv_num_cutoff,silent)
+
+	seqResetFilter(genofile)
+
+	return(results)
+}
